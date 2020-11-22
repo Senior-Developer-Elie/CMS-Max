a4b24b5d204acf1df87689096a5220e9ff697924
Senior-Developer-Elie <eliekassis.dev@gmail.com>
2020-11-23 05:57:42 +1000

    Update titles

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>CRM Dashboard - Evolution Marketing | Forgot Password</title>
+  <title>Evolution Marketing - CRM Dashboard | Forgot Password</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
