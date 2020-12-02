6a9475bde3d658394b53b6cf5ba5a2d46c1ff564
Senior-Developer-Elie <eliekassis.dev@gmail.com>
2020-12-03 05:34:24 +1000

    Fix google drive link missing notification

--- resources/views/dashboard-notifications/websites-missing-google-drive.blade.php
+++ resources/views/dashboard-notifications/websites-missing-google-drive.blade.php
@@ -1,4 +1,4 @@
 <div class="alert alert-danger alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    You have <a href="#" class="website-edit-link" data-website-id="{{ $firstWebsiteId }}">{{ $websitesMissingGoogleDriveCount }}</a> websites to add Google Drive link.
+    You have <a href="{{ route('websites.edit', $firstWebsiteId) }}" target="_blank">{{ $websitesMissingGoogleDriveCount }}</a> websites to add Google Drive link.
 </div>
