1f43b8735fd387b2dc67a4bd9991c9b6009a15a2
Senior-Developer-Elie <eliekassis.dev@gmail.com>
2021-05-03 07:21:43 -0500

    Add total budget to social media page

--- resources/views/manage-website/social-media/index.blade.php
+++ resources/views/manage-website/social-media/index.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.theme')
 
 @section('content-header')
-    <div class="mt-2">
-        <select id="websites-status-filter" style="width:200px;">
-            <option value="active" {{ $statusFilter == "active" ? 'selected' : '' }}>Active Websites</option>
-            <option value="inactive" {{ $statusFilter == "inactive" ? 'selected' : '' }}>Inactive Websites</option>
-        </select>
+    <div class="row social-media-page__filter-wrapper">
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="fas fa-dollar-sign"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Total Ad Spend</span>
+                  <span class="info-box-number">$ {{ prettyFloat($totalAdSpend) }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-info"><i class="fas fa-dollar-sign"></i></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Total Management Fee</span>
+                  <span class="info-box-number">$ {{ prettyFloat($totalManagementFee) }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+        </div>
     </div>
+    <select id="websites-status-filter" style="width:200px;">
+        <option value="active" {{ $statusFilter == "active" ? 'selected' : '' }}>Active Websites</option>
+        <option value="inactive" {{ $statusFilter == "inactive" ? 'selected' : '' }}>Inactive Websites</option>
+    </select>
 @endsection
 
 @if ($statusFilter == 'active')
