542edda524f0089803f4002a14bbc5683708d780
Senior-Developer-Elie <eliekassis.dev@gmail.com>
2021-05-23 17:56:18 -0500

    Make social total budget only visible to Super Admin and Mitch

--- resources/views/manage-website/social-media/index.blade.php
+++ resources/views/manage-website/social-media/index.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.theme')
 
 @section('content-header')
-    <div class="row social-media-page__filter-wrapper">
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="fas fa-dollar-sign"></i></span>
-                <div class="info-box-content">
-                  <span class="info-box-text">Total Ad Spend</span>
-                  <span class="info-box-number">$ {{ prettyFloat($totalAdSpend) }}</span>
+    @if (in_array(Auth::user()->id, [1, 2]))
+        <div class="row social-media-page__filter-wrapper">
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="info-box">
+                    <span class="info-box-icon bg-green"><i class="fas fa-dollar-sign"></i></span>
+                    <div class="info-box-content">
+                    <span class="info-box-text">Total Ad Spend</span>
+                    <span class="info-box-number">$ {{ prettyFloat($totalAdSpend) }}</span>
+                    </div>
+                    <!-- /.info-box-content -->
                 </div>
-                <!-- /.info-box-content -->
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="fas fa-dollar-sign"></i></span>
+                    <div class="info-box-content">
+                    <span class="info-box-text">Total Management Fee</span>
+                    <span class="info-box-number">$ {{ prettyFloat($totalManagementFee) }}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                </div>
             </div>
         </div>
-        <div class="col-12 col-md-4">
-            <div class="info-box">
-                <span class="info-box-icon bg-info"><i class="fas fa-dollar-sign"></i></span>
-                <div class="info-box-content">
-                  <span class="info-box-text">Total Management Fee</span>
-                  <span class="info-box-number">$ {{ prettyFloat($totalManagementFee) }}</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-        </div>
-    </div>
+    @endif
     <select id="websites-status-filter" style="width:200px;">
         <option value="active" {{ $statusFilter == "active" ? 'selected' : '' }}>Active Websites</option>
         <option value="inactive" {{ $statusFilter == "inactive" ? 'selected' : '' }}>Inactive Websites</option>
